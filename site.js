b0d9c53edcf12b11fff1e38a2908670784827089
Alex Zemak <123azemak@frhsd.com>
2023-03-03 08:05:08 -0500

    Milestone 3 and 4 done

--- site.js
+++ site.js
@@ -49,32 +49,67 @@ const vue_app = Vue.createApp({
                   // the switch statement will go through every json object, looking at each date in each movie, when it finds a match, it will take that date and convert it into us date format
                   // maybe make the code in the expression a number that goes up everytime it does not find the right movie, and loops back once it tries to go past the fifth movie
                   // to make the number go up, maybe make an if else statement? but why would i make an if else statement when the switch statement is meant to replace the need of an if else statement
-                  // 
-                  switch (/*idk wat to put here*/) {
-            case movies[index].released = "[1994, 10, 14]":
-                  movies[index].released = "December 10, 1987";
-              break;
-            case movies[index].released = "[1972, 3, 24]":
-                  movies[index].released = "March 24, 1972";
-              break;
-            case movies[index].released = "[1974, 12, 20]":
-                  movies[index].released = "December 20, 1974";
-              break;
-            case movies[index].released = "[2008, 7, 18]":
-                  movies[index].released = "July 18, 2008";
-              break;
-            case movies[index].released = "[1993, 11, 30]":
-                  movies[index].released = "November 30, 1993";
-              break;
+                  // oki i got it to work thanks to jesse
+                  switch (dateArray[1]) {
+            case 0:
+                  return "Error";
+                  break;
+                  case 1:
+                        return "January" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 2:
+                        return "Feburary" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 3:
+                        return "March" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 4:
+                        return "April" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 5:
+                        return "May" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 6:
+                        return "June" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 7:
+                        return "July" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 8:
+                        return "August" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 9:
+                        return "September" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 10:
+                        return "October" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 11:
+                        return "November" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        case 12:
+                        return "December" +" "+dateArray[2]+", "+ dateArray[0];
+                        break;
+                        default:
+                        return "Error";
+                        break;
                       }
             },
             posterClick(index){
-// var x = 
-// var y = 
+var x = this.movies[index].posterindex;
+var y = this.movies[index].posters.length;
+if(x < y-1) {
+      this.movies[index].posterindex++;
+}
+else {
+      this.movies[index].posterindex = 0;
+}
             },
             timeText(minutes){
-
-            }
+var hours = Math.trunc(minutes/60);
+var min = minutes % 60;
+return hours + "h" + min + "m"
+            },
       },
 
 
